796ec8f2e150035c2e85b798a0de50abfa2cc889
Christian Sherland <christiansherland@gmail.com>
2014-02-11 20:21:32 -0500

    fixed the import imaging library

--- src/main/java/edu/cooper/ece465/ImageClient.java
+++ src/main/java/edu/cooper/ece465/ImageClient.java
@@ -8,16 +8,16 @@ import java.net.*;
 import java.awt.image.*;
 import java.awt.*;
 
-import org.apache.commons.*;
+import org.apache.commons.imaging.*;
 
 public class ImageClient {
-    
+
     public ImageClient() {
         System.out.println("Image Client Initialized.");
     }
 
     public static int main(String[] args) {
-        
+
 
         return 0;
     }
