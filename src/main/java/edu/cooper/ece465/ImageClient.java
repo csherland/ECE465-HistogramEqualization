03b690f1b460eaf6fa1155984d690e392cf8820a
luster <ethan.lusterman@gmail.com>
2014-02-11 19:59:11 -0500

    added apache snapshots

--- src/main/java/edu/cooper/ece465/ImageClient.java
+++ src/main/java/edu/cooper/ece465/ImageClient.java
@@ -4,7 +4,18 @@
  */
 
 package ece465.cooper.edu;
+import java.net.*;
+import java.awt.image.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
 
 public class ImageClient {
+    
+    public ImageClient() {
+        System.out.println("Image Client Initialized.");
+    }
+
+    public void manipulateImage(String filename) {
+    }
 
 }
