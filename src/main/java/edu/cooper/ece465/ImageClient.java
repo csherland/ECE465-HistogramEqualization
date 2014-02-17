ac9f6951fb77536907e02da42d0b128728232405
Christian Sherland <christiansherland@gmail.com>
2014-02-17 14:47:47 -0500

    flickr api and a few other things

--- src/main/java/edu/cooper/ece465/ImageClient.java
+++ src/main/java/edu/cooper/ece465/ImageClient.java
@@ -4,10 +4,11 @@
  */
 
 package ece465.cooper.edu;
+import java.util.*;
 import java.net.*;
 import java.awt.image.*;
 import java.awt.*;
-
+import com.flickr4java.flickr.*;
 
 public class ImageClient {
 
@@ -16,6 +17,31 @@ public class ImageClient {
     }
 
     public static int main(String[] args) {
+
+        /*
+         * Connect to flickr
+         *  TODO: Change the key to be read from properties file and
+         *          add to .gitignore
+         */
+        String apiKey = "66f8567513744d2afa1019d05be88a17";
+        String secret = "30406da39c66d4af";
+        Flickr f = new Flickr(apiKey, secret, new REST());
+
+        /*
+         * Get a bunch of images
+         */
+
+
+        /*
+         * Send them to the server
+         */
+
+
+        /*
+         * Get back equalized images
+         */
+
+
         return 0;
     }
 
