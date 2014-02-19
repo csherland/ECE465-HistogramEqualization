46a4080fb9e98cc8fc411442b59975aad07527d6
mmscibor <mmscibor@gmail.com>
2014-02-18 19:11:15 -0500

    Added a helper class

---
+++ src/main/java/edu/cooper/ece465/Consumer.java
@@ -0,0 +1,11 @@
+pacakage edu.cooper.ece465;
+import java.util.*;
+
+public class Consumer extends Thread {
+    
+    public void run() {
+        while(true){
+            
+        }
+    }
+}
---
+++ src/main/java/edu/cooper/ece465/ImageHelper.java
@@ -0,0 +1,55 @@
+package edu.cooper.ece465;
+import java.util.*;
+
+public class ImageHelper {
+    
+    private BufferedImage image;
+    
+    /* Some flags for determining whether a process
+     * is a consumer or a producer */
+
+    private boolean equalized = false;
+    private boolean doneEqualizing = false;
+    private boolean availableUnequalized = false;
+
+    public synchronized void setDoneEqualizing(boolean value) {
+        doneEqualizing = value;
+    }
+
+    public synchronized boolean getDoneEqualizing() {
+        return doneEqualizing;
+    }
+
+    /* Deals with passing the image between the ImageProducer
+     * and the ImageConsumer. */
+
+    public synchronized BufferedImage getEqualized() {
+        while (availableUnequalized == false) {
+            if (doneEqualizing) {
+                return null;
+            }
+            
+            try {
+                wait();
+            } catch (InterruptedException e) { }
+            
+        }
+        
+        availableUnequalized = false;
+        notifyAll();
+        return image;
+    }
+
+    public synchronized void putUnequalized(BufferedImage newImage) {
+        while (availableUnequalized == true) {
+            try {
+                wait();
+            } catch (InterruptedException e) { }
+
+        }
+        
+        image = newImage;
+        availableUnequalized = false;
+        notifyAll();
+    }
+}
