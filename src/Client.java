dbbae18fdd6e289ee4a590259aad529b91015890
mmscibor <mmscibor@gmail.com>
2014-02-25 19:17:31 -0500

    client server works basically

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,6 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /* Client objects have images that need to be Equalized: they send a request
@@ -9,20 +9,25 @@ import java.net.Socket;
  */
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 2014);
+    public static void main(String[] args) {
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 9999);
 
-        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-        System.out.println("Input stream open: Client side.");
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            System.out.println("Input stream open: Client side.");
 
-        ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-        System.out.println("Output stream open: Client side.");
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Output stream open: Client side.");
 
-        output.writeBytes("HELLO\nJust in case.\nJust in case. \n Though, just in case.\n");
+            output.writeBytes("HELLO\nJust in case.\nJust in case. \n Though, just in case.\n");
 
-        output.close();
-        input.close();
-        socket.close();
+            output.close();
+            input.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Socket failed to initialize.");
+        }
     }
 
 }
--- src/Server.java
+++ src/Server.java
@@ -1,26 +1,25 @@
 
 /* Once connected with a Client, the Server handles Equalization. */
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(2014);
+            ServerSocket serverSocket = new ServerSocket(9999);
             Socket socket = serverSocket.accept();
 
-            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            DataInputStream input = new DataInputStream(socket.getInputStream());
             System.out.println("Input stream open: Server side.");
 
-            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
             System.out.println("Output stream open: Server side.");
 
             while (true) {
-
                 String ourMessage = input.readLine();
                 System.out.println(ourMessage);
                 if (ourMessage.equals("Just in case.")){
