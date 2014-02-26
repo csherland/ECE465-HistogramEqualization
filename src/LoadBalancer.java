583cfe7eb48f58063d307ab0aa8f6606ff694081
luster <ethan.lusterman@gmail.com>
2014-02-26 17:35:45 -0500

    pseudocode loadbalancer

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.PriorityQueue;
 
 /* LoadBalancer receives requests from Client objects to be connected to an
  * available Server. Tells the Client which Server is available. */
@@ -16,12 +17,24 @@ public class LoadBalancer {
         //
         // ServerListener
         // ServerLoadComparator
+        PriorityQueue<ServerStatus> queue = new PriorityQueue<ServerStatus>();
+        HashMap<String,ServerStatus> hm;
+        hm = new HashMap<String, ServerStatus>();
+        int portNumber = 9999;
+
+        Runnable listener = new ServerListener(queue, hm, portNumber);
+        listener.run();
 
         try {
             ServerSocket serverSocket = new ServerSocket(2014);
             while(true){
                 System.out.println("New Connection established.");
                 Socket socket = serverSocket.accept();
+
+                // look at best value in priority queue
+                // connect client with that server
+
+
 
             }
         } catch (IOException e) {
